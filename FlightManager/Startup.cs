c14f8877011f21c39452894c72c7f8e25e945323
Stoyan Haydushki <s.haidushky@abv.bg>
2021-04-07 13:49:44 +0300

    Scaffold Identity and change user type

--- FlightManager/Startup.cs
+++ FlightManager/Startup.cs
@@ -1,4 +1,5 @@
 using FlightManager.Data;
+using FlightManager.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,7 +31,7 @@ namespace FlightManager
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
