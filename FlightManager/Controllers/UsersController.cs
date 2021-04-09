1d20315f864976098b04407155db817ef830abee
Stoyan Haydushki <s.haidushky@abv.bg>
2021-04-09 11:49:03 +0300

    Add User Create and Index

---
+++ FlightManager/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using FlightManager.Areas.Identity.Pages.Account;
+using FlightManager.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightManager.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserManager<User> userManager;
+        public UsersController(UserManager<User> usrMgr)
+        {
+            this.userManager = usrMgr;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterModel.InputModel input)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    UserName = input.Email,
+                    Email = input.Email,
+                    Address = input.Address,
+                    FirstName = input.FirstName,
+                    LastName = input.LastName,
+                    PersonalNo = input.PersonalNo
+                };
+
+                var result = await userManager.CreateAsync(user, input.Password);
+                if (result.Succeeded)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(input);
+        }
+    }
+}
