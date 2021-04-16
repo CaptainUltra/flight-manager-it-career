21378d2f238a3a633b120787d0d1f8623c34dae0
Stoyan Haydushki <s.haidushky@abv.bg>
2021-04-16 16:58:15 +0300

    Add DataAnnotations to Passenger model

---
+++ FlightManager/Data/Migrations/20210416135648_UpdatePassengersTableRequired.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlightManager.Data.Migrations
+{
+    public partial class UpdatePassengersTableRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Passengers_PersonalNo",
+                table: "Passengers");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b94fb690-4eb2-4a7d-baa5-8cd545b48764");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "deb4670f-e7c5-431a-a671-ecf10e8e8db0");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Passengers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalNo",
+                table: "Passengers",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nationality",
+                table: "Passengers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MiddleName",
+                table: "Passengers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Passengers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Passengers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "680c94e0-51d4-4594-9ab1-051c4a014970", "0b312bfe-c3f8-428a-917a-f4f8af6b404e", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "545a6e9d-98a6-4f99-9eed-689103b14bce", "006cfabc-d36c-40e4-9882-7b6008c895c1", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Passengers_PersonalNo",
+                table: "Passengers",
+                column: "PersonalNo",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Passengers_PersonalNo",
+                table: "Passengers");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "545a6e9d-98a6-4f99-9eed-689103b14bce");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "680c94e0-51d4-4594-9ab1-051c4a014970");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalNo",
+                table: "Passengers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nationality",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MiddleName",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b94fb690-4eb2-4a7d-baa5-8cd545b48764", "65cad501-42fb-4584-87ff-5cf68357fbf8", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "deb4670f-e7c5-431a-a671-ecf10e8e8db0", "1f237daf-dfb2-45f5-b63f-190055f14e20", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Passengers_PersonalNo",
+                table: "Passengers",
+                column: "PersonalNo",
+                unique: true,
+                filter: "[PersonalNo] IS NOT NULL");
+        }
+    }
+}
--- FlightManager/Data/Models/Passenger.cs
+++ FlightManager/Data/Models/Passenger.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FlightsManager.Data.Models
 {
@@ -9,12 +10,21 @@ namespace FlightsManager.Data.Models
             this.Reservations = new List<PassengerReservation>();
         }
         public int Id { get; set; }
+        [Required]
+        [StringLength(10)]
         public string PersonalNo { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string MiddleName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [Phone]
         public string Telephone { get; set; }
+        [Required]
         public string Nationality { get; set; }
+        [Required]
 
         public List<PassengerReservation> Reservations { get; set; }
     }
