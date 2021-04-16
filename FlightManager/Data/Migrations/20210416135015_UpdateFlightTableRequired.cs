1532ea16aeeb0d195c8fcd09fcf21471785899ec
Stoyan Haydushki <s.haidushky@abv.bg>
2021-04-16 16:53:49 +0300

    Add Required attributes to Flight

---
+++ FlightManager/Data/Migrations/20210416135015_UpdateFlightTableRequired.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlightManager.Data.Migrations
+{
+    public partial class UpdateFlightTableRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "90b1907a-f51f-4aeb-b95f-d202e04f59ca");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b8dd3b2e-920a-4d3d-ace1-c4de3f5b661f");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "To",
+                table: "Flights",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PilotName",
+                table: "Flights",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "From",
+                table: "Flights",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlightNumber",
+                table: "Flights",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AircraftType",
+                table: "Flights",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b94fb690-4eb2-4a7d-baa5-8cd545b48764", "65cad501-42fb-4584-87ff-5cf68357fbf8", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "deb4670f-e7c5-431a-a671-ecf10e8e8db0", "1f237daf-dfb2-45f5-b63f-190055f14e20", "Employee", "EMPLOYEE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b94fb690-4eb2-4a7d-baa5-8cd545b48764");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "deb4670f-e7c5-431a-a671-ecf10e8e8db0");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "To",
+                table: "Flights",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PilotName",
+                table: "Flights",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "From",
+                table: "Flights",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlightNumber",
+                table: "Flights",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AircraftType",
+                table: "Flights",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b8dd3b2e-920a-4d3d-ace1-c4de3f5b661f", "81dbf9ec-f225-4d4c-b04f-f9cbd35b2794", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "90b1907a-f51f-4aeb-b95f-d202e04f59ca", "c27f46fe-0c21-4d6d-a046-6be2647e608f", "Employee", "EMPLOYEE" });
+        }
+    }
+}
--- FlightManager/Data/Models/Flight.cs
+++ FlightManager/Data/Models/Flight.cs
@@ -6,17 +6,26 @@ namespace FlightsManager.Data.Models
     public class Flight
     {
         public int Id { get; set; }
+        [Required]
         public string From { get; set; }
+        [Required]
         public string To { get; set; }
 
+        [Required]
         [DataType(DataType.DateTime)]
         public DateTime DepartureDateTime { get; set; }
+        [Required]
         [DataType(DataType.DateTime)]
         public DateTime ArrivalDateTime { get; set; }
+        [Required]
         public string AircraftType { get; set; }
+        [Required]
         public string FlightNumber { get; set; }
+        [Required]
         public string PilotName { get; set; }
+        [Required]
         public int PassengerCapacity { get; set; }
+        [Required]
         public int BusinessCapacity { get; set; }
     }
 }
